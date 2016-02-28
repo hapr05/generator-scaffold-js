45e597f362dd14a9bf16baeb09df4fb14cd58f02
James Goddard <james@goddard.bz>
2016-02-28 13:12:55 -0600

    adding integration testing

---
+++ test/integration/run.js
@@ -0,0 +1,66 @@
+(function (require, process) {
+	'use strict';
+
+	const spawn = require ('child_process').spawn,
+		path = require ('path'),
+		helpers = require ('yeoman-test'),
+		temp = require ('temp'),
+		testDir = 'oldschool_test',
+		prompts = {
+			name: 'test-app',
+			description: 'test-description',
+			homepage: 'test-homepage',
+			bugs: 'test-issues',
+			license: 'MIT',
+			cName: 'test-name',
+			cEmail: 'test-email',
+			cUrl: 'test-url',
+			repository: 'test-repository'
+		};
+
+
+	function create () {
+		return new Promise ((resolve, reject) => {
+			console.info ('Creating test directory');
+			temp.mkdir (testDir, (e, dir) => {
+				if (e) {
+					reject (e);
+				} else {
+					let yo;
+
+					console.info (`Test directory created: ${ dir }`);
+					process.chdir (dir);
+
+					console.info ('Generating app');
+					helpers.run (path.join ( __dirname, '../../app')).withPrompts (prompts).inDir (dir).on ('end', (code) => {
+						console.info ('Running npm install');
+						spawn ('npm', [ 'install' ]).on ('close', (code) => {
+							if (code) {
+								reject (`failed to install npm modules: ${ code }`);
+							} else {
+								console.info ('Running gulp js (will eventually replace)');
+								spawn ('gulp', [ 'js' ]).on ('close', (code) => {
+									if (code) {
+										reject (`gulp failed: ${ code }`);
+									} else {
+										resolve ();
+									}
+								});
+							}
+						});
+					}).on ('error', (e) => {
+						reject ('failed to generate: ' + e);
+					});
+				}
+			});
+		});
+	}
+
+	temp.track ();
+	create ().then (() => {
+		process.exit (0);
+	}, (e) => {
+		console.error (e);
+		process.exit (1);
+	});
+} (require, process));
---
+++ test/unit/util/validators.js
@@ -0,0 +1,24 @@
+(function (require) {
+	'use strict';
+
+	const assert = require ('assert'),
+		validators = require ('../../../util/validators');
+
+	describe ('validators', () => {
+		describe ('required', () => {
+			it ('should validate non-emty string', () => {
+				assert.strictEqual (validators.required ('test'), true);
+			});
+
+			it ('should fail emty string', () => {
+				assert.strictEqual (validators.required (''), false);
+			});
+
+			it ('should fail non-string', () => {
+				assert.strictEqual (validators.required (1), false);
+			});
+		});
+	});
+} (require));
+
+
